f8a322027953beac6a1a2ebd4f063e4bd9010e9a
ExSchYte <up201707427@fe.up.pt>
2024-03-15 09:52:07 +0000

    Lab3 done

--- lab3/keyboard.c
+++ lab3/keyboard.c
@@ -4,7 +4,7 @@
 #include <lcom/lcf.h>
 #include <stdint.h>
 
-int hook_id = 1;
+int kbd_hook_id = 1;
 uint8_t kbd_outbuf;
 
 void(kbc_ih)() {
@@ -22,12 +22,12 @@ void(kbc_ih)() {
 int(keyboard_subscribe_int)(uint8_t *bit_no) {
   if (bit_no == NULL)
     return 1;
-  *bit_no = BIT(hook_id);
-  return sys_irqsetpolicy(KEYBOARD_IRQ, IRQ_REENABLE | IRQ_EXCLUSIVE, &hook_id);
+  *bit_no = BIT(kbd_hook_id);
+  return sys_irqsetpolicy(KEYBOARD_IRQ, IRQ_REENABLE | IRQ_EXCLUSIVE, &kbd_hook_id);
 }
 
 int(keyboard_unsubscribe_int)() {
-  return sys_irqrmpolicy(&hook_id);
+  return sys_irqrmpolicy(&kbd_hook_id);
 }
 
 int can_read_outbuf() {
@@ -60,45 +60,49 @@ int can_read_outbuf() {
   return 0;
 }
 
-int write_to_kbc(uint8_t port, uint8_t cmdbyte){
+int write_to_kbc(uint8_t port, uint8_t cmdbyte) {
   uint8_t st;
-  int attempts=10;
-  while( attempts>0 ) { 
-    if(util_sys_inb(ST_REGISTER, &st)!=0){
+  int attempts = 10;
+  while (attempts > 0) {
+    if (util_sys_inb(ST_REGISTER, &st) != 0) {
       return 1;
-    } /* assuming it returns OK */ /* loop while 8042 input buffer is not empty */ 
-    if( (st & IBF) == 0 ) {  //if input buffer is not full
-      if(sys_outb(port, cmdbyte)!=0){
+    } /* assuming it returns OK */ /* loop while 8042 input buffer is not empty */
+    if ((st & IBF) == 0) {         // if input buffer is not full
+      if (sys_outb(port, cmdbyte) != 0) {
         return 1;
-      } /* no args command */ 
-      return 0; 
-      } 
-      tickdelay(micros_to_ticks(20000));              
-      attempts--;
+      } /* no args command */
+      return 0;
     }
-    return 1; //if attempts end, throw error
+    tickdelay(micros_to_ticks(20000));
+    attempts--;
+  }
+  return 1; // if attempts end, throw error
 }
-int read_value_data_from_kbc(uint8_t port,uint8_t *output){
-  int attempts=10;
-  while( attempts>0 ) { 
-      if(can_read_outbuf()==0){
-        if (util_sys_inb(OUT_BUF, output) == 0){  //read data from output buffer
-            return 0;
-        }
+int read_value_data_from_kbc(uint8_t port, uint8_t *output) {
+  int attempts = 10;
+  while (attempts > 0) {
+    if (can_read_outbuf() == 0) {
+      if (util_sys_inb(OUT_BUF, output) == 0) { // read data from output buffer
+        return 0;
       }
-      tickdelay(micros_to_ticks(20000));
-        attempts--;
     }
-    return 1; //if attempts end, throw error
+    tickdelay(micros_to_ticks(20000));
+    attempts--;
+  }
+  return 1; // if attempts end, throw error
 }
-int restore_interrupts(){
+int restore_interrupts() {
   uint8_t cmdbyte;
-  if(write_to_kbc(KBC_CMD_REG,READ_CMD_BYTE)) return 1; //telling kbc we are going to read command byte
-  if(read_value_data_from_kbc(OUT_BUF, &cmdbyte)) return 1; //retrieving command byte from output buffer
-  printf("%x",cmdbyte);
-  cmdbyte|=ENABLE_INTERRUPT;
-  printf("%x",cmdbyte); //enabling interrupts on command byte
-  if(write_to_kbc(KBC_CMD_REG,WRITE_CMD_BYTE)) return 1; //telling kbc we are going to write command byte
-  if(write_to_kbc(WRITE_CMD_BYTE,cmdbyte)) return 1; //write the command byte
+  if (write_to_kbc(KBC_CMD_REG, READ_CMD_BYTE))
+    return 1; // telling kbc we are going to read command byte
+  if (read_value_data_from_kbc(OUT_BUF, &cmdbyte))
+    return 1; // retrieving command byte from output buffer
+  printf("%x", cmdbyte);
+  cmdbyte |= ENABLE_INTERRUPT;
+  printf("%x", cmdbyte); // enabling interrupts on command byte
+  if (write_to_kbc(KBC_CMD_REG, WRITE_CMD_BYTE))
+    return 1; // telling kbc we are going to write command byte
+  if (write_to_kbc(WRITE_CMD_BYTE, cmdbyte))
+    return 1; // write the command byte
   return 0;
 }
--- lab3/lab3.c
+++ lab3/lab3.c
@@ -1,13 +1,16 @@
 #include <lcom/lcf.h>
 
 #include "keyboard.h"
+#include "timer.h"
 #include <i8042.h>
+#include <i8254.h>
 #include <lcom/lab3.h>
 #include <stdbool.h>
-#include <stdint.h>
+#include <stdint.h>m
 
 extern int sys_inb_counter;
 extern uint8_t kbd_outbuf;
+extern int global_counter;
 
 int main(int argc, char *argv[]) {
   // sets the language of LCF messages (can be either EN-US or PT-PT)
@@ -34,12 +37,12 @@ int main(int argc, char *argv[]) {
 }
 
 int(kbd_test_scan)() {
-  uint8_t bit_no = 0x01, bytes[2];
+  uint8_t kbd_bit_no = 0x01, bytes[2];
   int ipc_status, r, size = 0;
   message msg;
   bool make = false;
 
-  if ((keyboard_subscribe_int(&bit_no)) != 0) {
+  if ((keyboard_subscribe_int(&kbd_bit_no)) != 0) {
     return 1;
   }
 
@@ -52,7 +55,7 @@ int(kbd_test_scan)() {
     if (is_ipc_notify(ipc_status)) {
       switch (_ENDPOINT_P(msg.m_source)) {
         case HARDWARE:
-          if (msg.m_notify.interrupts & bit_no) {
+          if (msg.m_notify.interrupts & kbd_bit_no) {
             kbc_ih();
             if (size == 0) {
               bytes[0] = kbd_outbuf;
@@ -102,12 +105,74 @@ int(kbd_test_poll)() {
   uint8_t bytes[2];
   int size = 0;
   bool make = false;
-  while(kbd_outbuf != ESC_BREAK){
-      if(can_read_outbuf()==0){
-        if (util_sys_inb(OUT_BUF, &kbd_outbuf) != 0) {
+  while (kbd_outbuf != ESC_BREAK) {
+    if (can_read_outbuf() == 0) {
+      if (util_sys_inb(OUT_BUF, &kbd_outbuf) != 0) {
         return 1;
+      }
+      if (size == 0) {
+        bytes[0] = kbd_outbuf;
+        size++;
+        if (kbd_outbuf == 0xE0) {
+          continue;
         }
-        if (size == 0) {
+        else {
+          if ((kbd_outbuf & BIT(7)) != 0) {
+            make = false;
+          }
+          else {
+            make = true;
+          }
+        }
+      }
+      else {
+        bytes[1] = kbd_outbuf;
+        size++;
+        if ((kbd_outbuf & BIT(7)) != 0) {
+          make = false;
+        }
+        else {
+          make = true;
+        }
+      }
+      if (kbd_print_scancode(make, size, bytes) != 0) {
+        return 1;
+      }
+      size = 0;
+    }
+  }
+  if (restore_interrupts())
+    return 1;
+  return 0;
+}
+
+int(kbd_test_timed_scan)(uint8_t n) {
+  uint8_t kbd_bit_no = 0x01, timer_bit_no = 0x00, bytes[2];
+  int ipc_status, r, size = 0, last_key_time = 0;
+  message msg;
+  bool make = false;
+  if (timer_subscribe_int(&timer_bit_no) != 0)
+    return 1;
+  if ((keyboard_subscribe_int(&kbd_bit_no)) != 0) {
+    return 1;
+  }
+
+  while (kbd_outbuf != ESC_BREAK && ((global_counter - last_key_time) < (n * 60))) {
+    /* Get a request message. */
+    if ((r = driver_receive(ANY, &msg, &ipc_status)) != 0) {
+      printf("driver_receive failed with: %d", r);
+      continue;
+    }
+    if (is_ipc_notify(ipc_status)) {
+      switch (_ENDPOINT_P(msg.m_source)) {
+        case HARDWARE:
+          if (msg.m_notify.interrupts & timer_bit_no) {
+            timer_int_handler();
+          }
+          if (msg.m_notify.interrupts & kbd_bit_no) {
+            last_key_time = global_counter;
+            kbc_ih();
+            if (size == 0) {
               bytes[0] = kbd_outbuf;
               size++;
               if (kbd_outbuf == 0xE0) {
@@ -136,16 +201,20 @@ int(kbd_test_poll)() {
               return 1;
             }
             size = 0;
+          }
+          break;
+        default:
+          break;
       }
+    }
+    else { /* received a standard message, not a notification */
+      /* no standard messages expected: do nothing */
+    }
   }
-  if(restore_interrupts()) return 1;
-  printf("line 142");
-  return 0;
-}
+  kbd_print_no_sysinb(sys_inb_counter);
 
-int(kbd_test_timed_scan)(uint8_t n) {
-  /* To be completed by the students */
-  printf("%s is not yet implemented!\n", __func__);
+  if (timer_unsubscribe_int() != 0)
+    return 1;
 
-  return 1;
+  return keyboard_unsubscribe_int();
 }
