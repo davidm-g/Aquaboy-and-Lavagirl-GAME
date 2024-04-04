789c97ce5c3e07a35a9f8ec277c3154983d5a36a
Torpedo <tomas1.cmenoita@gmail.com>
2024-04-04 21:52:11 +0100

    lab5.c and makefile added

--- lab4/lab4.c
+++ lab4/lab4.c
@@ -11,7 +11,15 @@ extern struct packet packet;
 extern uint8_t kbd_outbuf;
 extern int byte_counter;
 extern int global_counter;
+typedef enum {
+  START,
+  UP,
+  VERTEX,
+  DOWN,
+  END
+} SystemState;
 
+SystemState state = START;
 int main(int argc, char *argv[]) {
   // sets the language of LCF messages (can be either EN-US or PT-PT)
   lcf_set_language("EN-US");
@@ -125,10 +133,79 @@ int(mouse_test_async)(uint8_t idle_time) {
   return mouse_unsubscribe_int();
 }
 
+uint16_t x_len_total = 0;
+uint16_t y_len_total = 0;
+void (update_state_machine)(uint8_t tolerance,uint8_t x_len) {
+
+    switch (state) {
+
+      case START:
+
+          // transição I
+          // se só o left-button estiver pressionado
+          if (packet.lb && !packet.rb && !packet.mb) {
+            state = UP;
+          }
+
+          break;
+
+      case UP:
+          //TODO: transições II, III e F
+          break;
+
+      case VERTEX:
+          //TODO: transições IV e F
+          break;
+
+      case DOWN:
+          //TODO: transições V, VI e F
+          break;
+
+      case END:
+          break;
+    }
+
+    
+}
 int(mouse_test_gesture)(uint8_t x_len, uint8_t tolerance) {
-  /* To be completed */
-  printf("%s: under construction\n", __func__);
-  return 1;
+  uint8_t bit_no = 0x02;
+  int ipc_status, r;
+  message msg;
+  if (mouse_subscribe_int(&bit_no) != 0)
+    return 1;
+  send_cmd_mouse(ENABLE_DATA);
+   //user version of mouse_enable_data_reporting
+  while (state != END) { /* You may want to use a different condition */
+    /* Get a request message. */
+    if ((r = driver_receive(ANY, &msg, &ipc_status)) != 0) {
+      printf("driver_receive failed with: %d", r);
+      continue;
+    }
+    if (is_ipc_notify(ipc_status)) { /* received notification */
+      switch (_ENDPOINT_P(msg.m_source)) {
+        case HARDWARE: /* hardware interrupt notification */
+          if (msg.m_notify.interrupts & bit_no) {
+            mouse_ih();
+            bytes_to_packet(); //put the byte in the packet, making sure its syncronized
+            if(byte_counter==3){ //we can now assemble the packet
+              packet_parse();
+              mouse_print_packet(&packet);
+              update_state_machine(tolerance,x_len);
+              byte_counter=0;
+              
+            }
+          }
+          break;
+        default:
+          break; /* no other notifications expected: do nothing */
+      }
+    }
+    else { /* received a standard message, not a notification */
+      /* no standard messages expected: do nothing */
+    }
+  }
+  send_cmd_mouse(DISABLE_DATA); //disable data reporting
+  return mouse_unsubscribe_int();
 }
 
 int(mouse_test_remote)(uint16_t period, uint8_t cnt) {
